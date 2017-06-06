b2794ee82a106548a90ceb6b0a1e0e0046418d63
Christian Kniep <christian@qnib.org>
2017-06-06 14:08:06 +0200

    Bump version: 1.1.0 → 1.1.1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ETC metrics collector based on qframe, inspired by qcollect,logstash and fullerite"
 	app.Usage = "gcollect-metrics [options]"
-	app.Version = "1.1.0"
+	app.Version = "1.1.1"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "host",
