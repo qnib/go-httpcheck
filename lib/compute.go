3e77b07e18821d748681ce96f0e00cf2af0ab5ce
Christian Kniep <ckniep@gaikai.com>
2017-06-06 11:04:00 +0200

    compute pi on /pi

---
+++ lib/compute.go
@@ -0,0 +1,21 @@
+package httpcheck
+
+import (
+	"math"
+)
+
+func pi(n int) float64 {
+	ch := make(chan float64)
+	for k := 0; k <= n; k++ {
+		go term(ch, float64(k))
+	}
+	f := 0.0
+	for k := 0; k <= n; k++ {
+		f += <-ch
+	}
+	return f
+}
+
+func term(ch chan float64, k float64) {
+	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
+}
--- lib/routes.go
+++ lib/routes.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"strconv"
 )
 
 type Health struct {
@@ -23,13 +24,36 @@ func NewHealth(n string, s int) Health {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
 	sAdd := strings.Split(r.RemoteAddr, ":")
 	fmt.Fprint(w, fmt.Sprintf("Welcome: %s\n", sAdd[0]))
-	msg := fmt.Sprintf("request:+1|c client_ip=%s,endpoint=index\n", sAdd[0])
-	fmt.Println(msg)
+	LogRequest(r, "/", now)
 }
 
 func ShowHealth(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
 	health := NewHealth("http", 200)
 	json.NewEncoder(w).Encode(health)
+	LogRequest(r, "/health", now)
+}
+
+func LogRequest(r *http.Request,ep string, start time.Time) {
+	now := time.Now()
+	dur := now.Sub(start).Nanoseconds()/1000000
+	sAdd := strings.Split(r.RemoteAddr, ":")
+	msg := fmt.Sprintf("request:+1|c client_ip=%s,endpoint=%s\n", sAdd[0], ep)
+	fmt.Printf(msg)
+	msg = fmt.Sprintf("duration:%d|ms client_ip=%s,endpoint=%s\n", dur, sAdd[0], ep)
+	fmt.Printf(msg)
+}
+
+
+func ComputePi(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	num := strings.TrimPrefix(r.URL.Path, "/pi/")
+	n, _ := strconv.Atoi(num)
+	res := pi(n)
+	//sAdd := strings.Split(r.RemoteAddr, ":")
+	fmt.Fprint(w, fmt.Sprintf("Welcome: pi(%s)=%.f\n", num, res))
+	LogRequest(r, "/pi", now)
 }
